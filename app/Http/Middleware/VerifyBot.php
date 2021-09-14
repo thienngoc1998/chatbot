cd3390d29cedad7666b830e16dda318e5c2b728d
tuyennh <tuyen.nguyen@kiaisoft.com>
2021-09-14 16:53:01 +0700

    dd

--- app/Http/Middleware/VerifyBot.php
+++ app/Http/Middleware/VerifyBot.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class VerifyBot
 {
@@ -16,7 +17,7 @@ class VerifyBot
      */
     public function handle(Request $request, Closure $next)
     {
-        dd($request);
+        Log::info('test', $request->all());
         if ($request->input("hub_mode") === "subscribe"
             && $request->input("hub_verify_token") === env("MESSENGER_VERIFY_TOKEN")) {
             return response($request->input("hub_challenge"), 200);
