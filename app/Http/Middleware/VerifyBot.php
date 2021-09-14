c29144c62f3a8c5f96a501755235cecdb1034f20
tuyennh <tuyen.nguyen@kiaisoft.com>
2021-09-14 16:22:42 +0700

    add

--- app/Http/Middleware/VerifyBot.php
+++ app/Http/Middleware/VerifyBot.php
@@ -16,6 +16,7 @@ class VerifyBot
      */
     public function handle(Request $request, Closure $next)
     {
+        dd($request);
         if ($request->input("hub_mode") === "subscribe"
             && $request->input("hub_verify_token") === env("MESSENGER_VERIFY_TOKEN")) {
             return response($request->input("hub_challenge"), 200);
